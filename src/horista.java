c1f35e5857f0e924eabba005f29e73a9870520e6
ronaldocandido99 <52680677+ronaldocandido99@users.noreply.github.com>
2021-03-23 23:05:49 -0300

    Add files via upload

---
+++ src/horista.java
@@ -0,0 +1,8 @@
+public class horista extends employee{
+
+    int qtd_horas;
+
+    public horista(String name, int qtd_horas){
+        this.qtd_horas = qtd_horas;
+    }
+}
---
+++ src/sindicato.java
@@ -0,0 +1,11 @@
+public class sindicato{
+    String servico_realizado;
+    String  ind_empregado_sindicato;
+    int valor_servico;
+
+    public sindicato(String nome, String ind_empregado_sindicato, int valor_servico) {
+        this.servico_realizado = servico_realizado;
+        this.ind_empregado_sindicato = ind_empregado_sindicato;
+        this.valor_servico = valor_servico;
+    }
+}
